359727737b2eb2a4efc0c8364623f93e1f4cfca4
Tetiana Dudarenko <tanya.dudarenko@gmail.com>
2021-03-07 13:46:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace perceptron_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
